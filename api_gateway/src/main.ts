4d23e044fbd838f7d0667047d96a7e17d3f754db
shalu2001 <danthanarayanas.21@uom.lk>
2026-02-17 17:47:53 +0530

    Comment out HTTPS options in NestFactory initialization

--- api_gateway/src/main.ts
+++ api_gateway/src/main.ts
@@ -3,15 +3,15 @@ import { AppModule } from './app.module';
 import { ConfigService, initializeEncryptionKey } from '@charmbooking/common';
 import { RpcToHttpFilter } from './rcp-exception-filter';
 import { RpcErrorInterceptor } from './rcp-interceptor';
-import * as fs from 'fs';
+// import * as fs from 'fs';
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync('./secrets/private-key.pem'),
-    cert: fs.readFileSync('./secrets/public-certificate.pem'),
-  };
+  // const httpsOptions = {
+  //   key: fs.readFileSync('./secrets/private-key.pem'),
+  //   cert: fs.readFileSync('./secrets/public-certificate.pem'),
+  // };
   const app = await NestFactory.create(AppModule, {
-    httpsOptions,
+    // httpsOptions,
   });
   const configService = app.get(ConfigService);
 
